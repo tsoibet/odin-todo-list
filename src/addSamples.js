159c47974c5979782b6d6c072ccc898f480fefc0
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-11-15 18:17:07 +0900

    Update samples

--- src/addSamples.js
+++ src/addSamples.js
@@ -1,11 +1,14 @@
+import { addWeeks, format } from "date-fns";
 import Todo from "./classTodo.js";
 
 export default function addSamples(todoList, projectList) {
 
-    let sampleTodo1 = new Todo("Sample Title 1", "sample 1", "2022-11-11", false, false, projectList.allProjects[0]);
-    let sampleTodo2 = new Todo("Sample Title 2", "sample 2", "2022-11-17", false, true, projectList.allProjects[1]);
-    let sampleTodo3 = new Todo("Sample Title 3", "sample 3", "2022-11-18", true, false, projectList.allProjects[2]);
-    let sampleTodo4 = new Todo("Sample Title 4", "sample 4", "2022-10-19", true, false, projectList.allProjects[2]);
+    const today = new Date();
+
+    let sampleTodo1 = new Todo("(Sample) Workout", "abs & upper arms", format(today, "yyyy-MM-dd"), false, false, projectList.allProjects[0]);
+    let sampleTodo2 = new Todo("(Sample) Finish Project: Todo List", "The Odin Project", "2022-11-15", true, true, projectList.allProjects[1]);
+    let sampleTodo3 = new Todo("(Sample) Move on to next chapter", "Linting", "", false, false, projectList.allProjects[1]);
+    let sampleTodo4 = new Todo("(Sample) Write weekly report", "", format(addWeeks(today, 1), "yyyy-MM-dd"), true, false, projectList.allProjects[2]);
     
     todoList.allTodos = [sampleTodo1, sampleTodo2, sampleTodo3, sampleTodo4];
 
